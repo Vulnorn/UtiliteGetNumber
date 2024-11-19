75235f8093ce46baa228535e5b5742a339f77db4
Vulnorn <mixadrbk31@gmail.com>
2024-11-19 10:14:05 +0300

    перестановка

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@
                 Console.WriteLine($"Число - {number}");
             }
 
+            public static void ShowReceivedPositiveNumber()
+            {
+                int number = GetPositiveNumber();
+                Console.WriteLine($"Число - {number}");
+            }
+
             public static int GetNumber(int enterNumber = 0)
             {
                 bool isEnterNumber = true;
@@ -26,30 +32,6 @@
                 }
 
                 return enterNumber;
-            }
-
-            public static bool TryGetNumber(out int numder)
-            {
-                string userInput;
-
-                do
-                {
-                    userInput = Console.ReadLine();
-                }
-                while (TryGetInputValue(userInput, out numder));
-
-                return true;
-            }
-
-            private static bool TryGetInputValue(string input, out int number)
-            {
-                if (int.TryParse(input, out number) == false)
-                {
-                    Console.WriteLine("Не корректный ввод.");
-                    return true;
-                }
-
-                return false;
             }
 
             public static int GetPositiveNumber(int enterPositiveNumber = 0)
@@ -74,6 +56,30 @@
                 }
 
                 return enterPositiveNumber;
+            }
+
+            public static bool TryGetNumber(out int numder)
+            {
+                string userInput;
+
+                do
+                {
+                    userInput = Console.ReadLine();
+                }
+                while (TryGetInputValue(userInput, out numder));
+
+                return true;
+            }
+
+            private static bool TryGetInputValue(string input, out int number)
+            {
+                if (int.TryParse(input, out number) == false)
+                {
+                    Console.WriteLine("Не корректный ввод.");
+                    return true;
+                }
+
+                return false;
             }
 
             private static bool GetNumberRange(int number)
