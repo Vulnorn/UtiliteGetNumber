f201b1b16260a170dd9329ce458dcb9e19a3340e
Vulnorn <mixadrbk31@gmail.com>
2024-11-19 16:37:02 +0300

    21

--- Program.cs
+++ Program.cs
@@ -32,14 +32,14 @@ namespace UtiliteGetNumber
 
                     if (int.TryParse(userInput, out enterNumber) == false)
                         Console.WriteLine("Не корректный ввод.");
-                    else if (GetNumberInRange(enterNumber, lowerLimitRangeNumbers, upperLimitRangeNumbers))
+                    else if (VerifyForAcceptableNumber(enterNumber, lowerLimitRangeNumbers, upperLimitRangeNumbers))
                         isEnterNumber = false;
                 }
 
                 return enterNumber;
             }
 
-            private static bool GetNumberInRange(int number, int lowerLimitRangeNumbers, int upperLimitRangeNumbers)
+            private static bool VerifyForAcceptableNumber(int number, int lowerLimitRangeNumbers, int upperLimitRangeNumbers)
             {
                 if (number < lowerLimitRangeNumbers)
                 {
